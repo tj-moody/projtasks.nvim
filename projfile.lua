ce1b7cf040a4b5b139a95391345f501d2f33600d
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-07-26 12:02:55 -0400

    feat: package plugin in setup call

---
+++ projfile.lua
@@ -0,0 +1,7 @@
+return {
+    ["version"] = "0.1.0",
+    ["tasks"] = {
+        ["run"] = [[echo task]],
+        ["test"] = [[echo test]],
+    },
+}
--- projtasks.lua
+++ projtasks.lua
@@ -1,7 +1,63 @@
-return {
-    ["version"] = "0.1.0",
-    ["tasks"] = {
-        ["run"] = [[echo task]],
-        ["test"] = [[echo test]],
-    },
-}
+proj_config = require('projfile')
+local Terminal = require('toggleterm.terminal').Terminal
+local M = {}
+
+M.setup = function(opts)
+    local term = Terminal:new({
+        hidden = true,
+        dir = "git_dir",
+        direction = "float",
+
+        on_open = function(term)
+            vim.cmd("startinsert!")
+            vim.api.nvim_buf_set_keymap(
+                term.bufnr,
+                "n", "q", "<cmd>close<CR>",
+                { noremap = true, silent = true }
+            )
+            vim.api.nvim_buf_set_keymap(
+                term.bufnr,
+                "t", "<C-T>", "<cmd>close<CR>",
+                { noremap = true, silent = true }
+            )
+        end,
+
+        -- on_close = function(term)
+        --     vim.cmd("startinsert!")
+        -- end,
+    })
+
+    vim.api.nvim_create_user_command(
+        "ProjtasksToggle",
+        function()
+            term:toggle()
+        end,
+        { desc = "Toggle Projtasks Terminal" }
+    )
+
+    vim.api.nvim_create_user_command(
+        "ProjtasksRun",
+        function()
+            term:open()
+            term:send(proj_config["tasks"]["run"], false)
+        end,
+        { desc = "Run Project" }
+    )
+
+    vim.api.nvim_create_user_command(
+        "ProjtasksTest",
+        function()
+            term:open()
+            term:send(proj_config["tasks"]["test"], false)
+        end,
+        { desc = "Test Project" }
+    )
+end
+
+-- TODO: Add neotest integration
+-- TODO: Add godbolt integration
+-- TODO: Add defaults based on project type and
+--       structure, ie `cargo run`, `cargo test`,
+--       etc. by default in Rust projects.
+
+return M
