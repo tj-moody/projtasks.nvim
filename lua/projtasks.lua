c1ab783e567da0e4272dea708b1af32f2c60f628
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-07-26 12:54:01 -0400

    fix: pcall properly

--- lua/projtasks.lua
+++ lua/projtasks.lua
@@ -1,4 +1,4 @@
-ok, proj_config = pcall(require('projfile'))
+ok, proj_config = pcall(require, 'projfile')
 if not ok then return end
 local Terminal = require('toggleterm.terminal').Terminal
 local M = {}
