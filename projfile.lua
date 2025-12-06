355a9b4c8ecbd53ddae4b0734244a6ad17e657b2
tj-moody <92702993+tj-moody@users.noreply.github.com>
2025-12-06 15:15:42 -0500

    ignore
    .

--- projfile.lua
+++
@@ -1,8 +0,0 @@
-return {
-    ["version"] = "0.1.1",
-    ["tasks"] = {
-        ["build"] = { [[echo "build"]] },
-        ["run"] = { [[echo "run"]] },
-        ["test"] = { [[echo "test"]] },
-    },
-}
